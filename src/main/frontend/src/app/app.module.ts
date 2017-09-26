2dbe098a3883d680214de02b5c584c6811582102
Michael Gulley <mgulley.mograph@gmail.com>
2017-09-26 17:29:44 -0400

    Adds tasks, tasks-list, tasks-add components and bootstrap

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksAddComponent } from './tasks/tasks-add/tasks-add.component';
+import { TasksListComponent } from './tasks/tasks-list/tasks-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TasksAddComponent,
+    TasksListComponent
   ],
   imports: [
     BrowserModule,
