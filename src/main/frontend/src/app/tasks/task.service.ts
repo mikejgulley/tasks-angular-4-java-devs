b67d4e3fbab46075011a6e077cc4ef2341a83d95
Michael Gulley <mgulley.mograph@gmail.com>
2017-09-26 19:08:08 -0400

    Implements saving of task state during change

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,6 +1,7 @@
 import {Http} from "@angular/http";
 import {Injectable} from "@angular/core";
 import "rxjs/add/operator/map";
+import {Task} from "app/tasks/task.model";
 
 @Injectable()
 export class TaskService {
@@ -12,6 +13,14 @@ export class TaskService {
     getTasks() {
         // this returns an observable from the rxjs library
         return this.http.get('/api/tasks').map(response => response.json());
+    }
+
+    saveTask(task: Task, checked: boolean) {
+        task.completed = checked;
+        return this.http.post('/api/tasks/save', task)
+            .map(
+                (response => response.json)
+            );
     }
 
 }
